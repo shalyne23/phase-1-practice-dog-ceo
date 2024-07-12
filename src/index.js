b52377f90e1f5a8af3e41a57fbc4b99c875f4a64
shalyne23 <shalynelangat@gmail.com>
2024-07-12 20:33:21 +0300

    practice dog ceo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchImage(){
+    fetch(imgUrl)
+    .then(res=>res.json())
+    .then(dogs=>{
+        dogs.message.forEach(dog => {
+            const image = document.createElement('img')
+           
+            image.src = dog.message
+
+            document.getElementById('dog-image-container').appendChild(image)
+        })
+    })
+
+}
+
+const breedUrl ='https://dog.ceo/api/breeds/list/all'
+function dogBreeds(){
+    fetch(breedUrl)
+    .then(res=>res.json())
+    .then(breeds=>breeds.message.forEach(breed=>{
+        console.log(breed)
+    }))
+}
+dogBreeds()
+window.addEventListener('load',fetchImage)
